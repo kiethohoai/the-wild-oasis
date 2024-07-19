272bc6d82008f8799e6355b757a63efe53f5b50f
kiethohoai <kietho.060193@gmail.com>
2024-07-19 08:59:39 +0700

    006 Global Styles With Styled Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,32 @@
 import React from "react";
 import styled from "styled-components";
+import GlobalStyle from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
 
 const H1 = styled.h1`
-    font-size: 60px;
-    font-weight: 600;
-    background-color: yellow;
+  font-size: 60px;
+  font-weight: 600;
+  background-color: yellow;
 `;
 
-const Button = styled.button`
-    font-size: 1.4rem;
-    padding: 1.2rem 1.6rem;
-    font-weight: 500;
-    border: none;
-    border-radius: 7px;
-    background-color: purple;
-    color: white;
-    cursor: pointer;
-    margin: 20px;
-`
-
-const Input = styled.input`
-    border: 1px solid #ddd;
-    border-radius: 5px;
-    padding: 0.8rem 1.2rem;
-
-`
-
 const StyleApp = styled.div`
-    background-color: orangered;
-    padding: 20px;
-`
+  background-color: orangered;
+  padding: 20px;
+`;
 
 function App() {
-    return (
-        <StyleApp>
-            <H1>Hello World</H1>
-            <Button onClick={() => alert("Check in")}>Check in</Button>
-            <Button onClick={() => alert("Check out")}>Check out</Button>
-            <br />
-            <Input type="number" placeholder="Number of guests"></Input>
-        </StyleApp>
-    );
+  return (
+    <>
+      <GlobalStyle />
+      <StyleApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert("Check in")}>Check in</Button>
+        <Button onClick={() => alert("Check out")}>Check out</Button>
+        <Input type="number" placeholder="Number of guests"></Input>
+      </StyleApp>
+    </>
+  );
 }
 
 export default App;
